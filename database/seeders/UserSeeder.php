e621deb83fa3565139e1b5454e08ae73b2f4d446
aldi-balkar <aldibalkar23@gmail.com>
2023-02-13 15:23:16 +0700

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = [
+            [
+                'name'           => 'Super Admin',
+                'email'          => 'aldibalkar23@gmail.com',
+                'password'       => Hash::make('12345678'),
+                'remember_token' => null,
+                'created_at'     => date('Y-m-d H:i:s'),
+                'updated_at'     => date('Y-m-d H:i:s'),
+            ],
+        ];
+
+        User::insert($user);
+    }
+}
